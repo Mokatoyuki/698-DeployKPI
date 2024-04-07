90d731d85342c77acc92ac8c3d756a085e8d91e9
Mokatoyuki <165541563+Mokatoyuki@users.noreply.github.com>
2024-04-07 15:54:00 +0700

    Update app_KPI-65130701915.py

--- app_KPI-65130701915.py
+++ app_KPI-65130701915.py
@@ -6,12 +6,9 @@ import seaborn as sns
 import pickle
 import gzip
 
-pip install --upgrade matplotlib
-
-
 # Load model and encoders
 with gzip.open('model-kpi-65130701915.pkl.gz', 'rb') as file:
-    model, department_encoder, region_encoder, education_encoder, gender_encoder, recruitment_channel_encoder = pickle.load(file)
+    data = pickle.load(file)
 
 model = data['model']
 department_encoder = data['department_encoder']
