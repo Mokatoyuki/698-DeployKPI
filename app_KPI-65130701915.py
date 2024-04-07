0a7a89d9922c8c233e4d776c19f28ab9c4783fa7
Mokatoyuki <165541563+Mokatoyuki@users.noreply.github.com>
2024-04-07 15:42:58 +0700

    Update app_KPI-65130701915.py

--- app_KPI-65130701915.py
+++ app_KPI-65130701915.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
+# Try importing matplotlib and seaborn
 try:
     import matplotlib.pyplot as plt
     import seaborn as sns
